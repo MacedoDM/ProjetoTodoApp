ab2498e33f0c7c015039340f4ce2e6e53c965add
Matheus D Macedo <mathduartemacedo@gmail.com>
2022-10-19 20:12:40 -0300

    Projeto todoApp

---
+++ app/src/main/java/ProjetoTarefas/App.java
@@ -0,0 +1,17 @@
+package ProjetoTarefas;
+
+import Controller.ProjectController;
+import Model.Project;
+import java.sql.Connection;
+import java.util.List;
+import util.ConnectionFactory;
+import Controller.TaskController;
+import Model.Task;
+import java.sql.Date;
+
+public class App {
+    public static void main(String[] args) {
+                
+        
+    }
+}
